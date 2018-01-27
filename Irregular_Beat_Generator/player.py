29cce764d6bdb6362b03499a7d8feec686687bdb
Bram Giesen <giesenbram@gmail.com>
2018-01-27 17:44:56 +0100

    adjusted player to work with time

--- Irregular_Beat_Generator/player.py
+++ Irregular_Beat_Generator/player.py
@@ -1,40 +1,35 @@
 import time
 import simpleaudio as sa
 
+
 kick  = '/Users/BramGiesen/Documents/HKU/CSD2a/EindopdrachtBeatG/kick.wav'
 snare = '/Users/BramGiesen/Documents/HKU/CSD2a/EindopdrachtBeatG/snare.wav'
 hihat = '/Users/BramGiesen/Documents/HKU/CSD2a/EindopdrachtBeatG/hihat.wav'
 
-# kickList = []
-# snareList = []
-# hihatList = []
 
 
-def play(k, s, h, beatsPerMeasure, tempo):
-	global kickList, snareList, hihatList
-	kickList = k
-	snareList = s
-	hihatList = h
-
-
-	a = 0
+def play(kickList, snareList, hihatList, beatsPerMeasure, tempo):
+	startTime = time.time()
+	triggerLenght = 0.2
+	index = 0
+	count = 0
 	if playbackLoop == True:
-		while playbackLoop:
+			while playbackLoop:
+				triggerTime = startTime + (triggerLenght * count)
 
-			for x in range(a, beatsPerMeasure):
-				x = x - a;
-				if kickList[x] == 1:
-					waveObj1 = sa.WaveObject.from_wave_file(kick)
-					playObj1 = waveObj1.play()
-				if snareList[x] == 1:
-					waveObj2 = sa.WaveObject.from_wave_file(snare)
-					playObj2 = waveObj2.play()
-				if hihatList[x] == 1:
-					waveObj3 = sa.WaveObject.from_wave_file(hihat)
-					playObj3 = waveObj3.play()
-					time.sleep(tempo)
-#
-# def clear():
-# 	del kickList[:]
-# 	del snareList[:]
-# 	del hihatList[:]
+				if time.time() > triggerTime:
+					if kickList[index] == 1:
+						waveObj1 = sa.WaveObject.from_wave_file(kick)
+						playObj1 = waveObj1.play()
+					if snareList[index] == 1:
+						waveObj2 = sa.WaveObject.from_wave_file(snare)
+						playObj2 = waveObj2.play()
+					if hihatList[index] == 1:
+						waveObj3 = sa.WaveObject.from_wave_file(hihat)
+						playObj3 = waveObj3.play()
+					index = index + 1
+					count = count + 1
+					if index == beatsPerMeasure:
+						index = 0
+					else:
+						time.sleep(0.01)
